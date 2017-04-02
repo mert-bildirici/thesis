9cdfc6b6e3d5587034611592ed8acdfdf176339c
mert-bildirici <mert-bildirici@users.noreply.github.com>
2017-04-02 23:49:33 +0300

    Add files via upload

--- data_analysis.R
+++ data_analysis.R
@@ -2,13 +2,11 @@ library(ggplot2)
 library(mice)
 
 #missing data percentages
-
 missing_data_g <- sum(is.na(data_1$`generation-1 (MW)`)) / nrow(data_1) * 100
 missing_data_l <- sum(is.na(data_1$`load-1 (MW)`)) / nrow(data_1) * 100
 
 #data analysis without imputing missing data
-
-  data_1 <- original_data
+data_1 <- original_data
 
   ##finding averages
   generation_average_1 <- sum(data_1$`generation-1 (MW)`, na.rm = T) / (nrow(data_1)-sum(is.na(data_1$`generation-1 (MW)`)))
@@ -49,9 +47,9 @@ missing_data_l <- sum(is.na(data_1$`load-1 (MW)`)) / nrow(data_1) * 100
         data_1$`new load (MW)`, 
         geom = "point",
         col = I("black"),
-        main = "Load-1", 
+        main = "Assumed Load", 
         xlab = "Data number", 
-        ylab = "Load (MW)")
+        ylab = "Assumed Load (MW)")
   
   qplot(data_1$`new load (MW)`,
         geom = "histogram",
@@ -59,6 +57,24 @@ missing_data_l <- sum(is.na(data_1$`load-1 (MW)`)) / nrow(data_1) * 100
         col = I("black"),
         fill = I("black"),
         alpha = I(.5),
-        main = "Distribution of Load-1", 
-        xlab = "Load (MW)", 
+        main = "Distribution of Assumed Load", 
+        xlab = "Assumed Load (MW)", 
+        ylab = "Frequency")
+  
+  qplot(c(1:nrow(data_1)), 
+        data_1$`power difference (MW)`, 
+        geom = "point",
+        col = I("black"),
+        main = "Power Difference", 
+        xlab = "Data number", 
+        ylab = "Power Difference (MW)")
+  
+  qplot(data_1$`power difference (MW)`,
+        geom = "histogram",
+        binwidth = 10,
+        col = I("black"),
+        fill = I("black"),
+        alpha = I(.5),
+        main = "Distribution of Power Difference", 
+        xlab = "Power Difference (MW)", 
         ylab = "Frequency")
