f0f912713b9467e350aeb635c4a7dd38042a0054
mert-bildirici <mert-bildirici@users.noreply.github.com>
2017-07-19 16:59:08 +0300

    thesis-19.07/1

--- HVAC.R
+++ HVAC.R
@@ -1,88 +1,82 @@
-#temperature function
-
-tempDynamics <- function(T_room, T_out, s, Q, R=50, C=9200){
-  
-  T_room_next <- T_room+((((T_out-T_room)/R)+(s*Q))*(600/C))
-  
-  return(T_room_next)
-}
-
-#temperature constraints check function
-
-constCheck <- function(T_room_next, T_min=22, T_max=26){
-  
-  if(T_room_next < T_min){
-    
-    return(2)
-  }
-  
-  else if(T_room_next > T_max){
-    
-    return(0)
-  }
-  
-  else{
-    
-    return(1)
-  }
-}
-
 #datas
 
-n <- 10
-P_set <- P_control/1000
+numberHVAC <- 50
+
+powerHVAC <- 0.003
+
+setPower <- x
+
+tempOut <- rep(10, length(setPower))
 
 #information of HVACs for one time step
 
-HVAC_data <- data.frame(id=1:n, T_room=24, T_out=15, Q=3, state=logical(length=n), control=numeric(length=n))
+tableHVAC <- data.frame(id=1:numberHVAC, tempRoom=22, state=integer(length=numberHVAC), control=integer(length=numberHVAC))
 
-result <- data.frame(step = integer(0), id = integer(0), room_temp = numeric(0), state = logical(0))
+resultHVAC <- data.frame(step=integer(0), id=integer(0), tempRoom=numeric(0), state=integer(0))
 
 #simulation
 
-for(j in 1:52704){
+for(j in 1:length(tempOut)){
+
+  setPowerRemain <- setPower[j]
   
-  HVAC_data[ ,3] <- T_out[j]
-  
-  P_set_remind <- P_set[j]
-  
-  for(i in 1:n){
-    
-    HVAC_data[i,2] <- tempDynamics(HVAC_data[i,2], HVAC_data[i,3], HVAC_data[i,5], HVAC_data[i,4])
-    
-    HVAC_data[i,6] <- constCheck(HVAC_data[i,2])
+  for(i in 1:numberHVAC){
+
+    tableHVAC[i,4] <- tempCheck(tableHVAC[i,2])
   }
   
-  HVAC_data <- HVAC_data[order(-HVAC_data$control, -HVAC_data$state), ]
+  tableHVAC <- tableHVAC[order(-tableHVAC$control, -tableHVAC$state), ]
   
-  for(i in 1:n){
+  for(i in 1:numberHVAC){
     
-    if(HVAC_data[i,6] == 2){
+    if(tableHVAC[i,4] == 1){
       
-      HVAC_data[i,5] = T 
+      tableHVAC[i,3] = 1
       
-      P_set_remind = P_set_remind - HVAC_data[i,4]
+      setPowerRemain = setPowerRemain - powerHVAC
     }
     
-    else if(HVAC_data[i,6] == 1 && P_set_remind > 0){
+    else if(tableHVAC[i,4] == 2){
       
-      HVAC_data[i,5] = T
+      tableHVAC[i,3] = 2
       
-      P_set_remind = P_set_remind - HVAC_data[i,4]
+      setPowerRemain = setPowerRemain - powerHVAC
     }
     
-    else{
+    else if(tableHVAC[i,4] == 0){
       
-      HVAC_data[i,5] = F
+      if(setPowerRemain > 0){
+        
+        if(tempOut < 22){
+          
+          tableHVAC[i,3] = 1
+        }
+        
+        else{
+          
+          tableHVAC[i,3] = 2
+        }
+        
+        setPowerRemain = setPowerRemain - powerHVAC
+      }
+      
+      else{
+        
+        tableHVAC[i,3] = 0
+      }
     }
+    
+    tableHVAC[i,2] <- tempDynamics(tempOut[j], tableHVAC[i,2], tableHVAC[i,3])
   }
   
-  P_out[j] <-  P_set[j] - P_set_remind
+  P_out[j] <-  setPower[j]-setPowerRemain
   
-  result <- rbind(result, data.frame(step=j, id=HVAC_data[ ,1], room_temp=HVAC_data[ ,2], state=HVAC_data[ ,5]))
+  resultHVAC <- rbind(resultHVAC, data.frame(step=j, id=tableHVAC[ ,1], tempRoom=tableHVAC[ ,2], state=tableHVAC[ ,3]))
   
-  if(j%%144 == 0){
+  if(j %% 1008 ==0){
     
-    print(j/144)
+    print(j/1008)
   }
 }
+
+resultHVAC <- resultHVAC[order(resultHVAC$id), ]
