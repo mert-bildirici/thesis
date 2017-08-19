22531b0d90e8fd8f40f6cb78a0415bbeab45bbd2
mert-bildirici <mert-bildirici@users.noreply.github.com>
2017-08-20 01:30:16 +0300

    scripts-20..08
    BESS üzerinde düzeltme yapıldı.

--- scripts/BESS.R
+++ scripts/BESS.R
@@ -2,7 +2,9 @@ library(ggplot2)
 library(reshape2)
 
 SoC <- numeric()
-SoC[1] <- 1
+SoC[1] <- 0.9
+
+setPowerBESS <- numeric(length(gridPower))
 
 for(i in 1:length(gridPower)){
   
@@ -11,45 +13,42 @@ for(i in 1:length(gridPower)){
     setPowerBESS[i] <- 0 
   }
   
-  else if(gridPower[i] > (lineCapacity*(1-lineSafetyMargin))){
+  else{
     
     setPowerBESS[i] <- -(gridPower[i]-(lineCapacity*(1-lineSafetyMargin)))
-    
-    if(setPowerBESS[i] < -powerBESS){
-      
-      setPowerBESS[i] <- -powerBESS
-    }
-  }
-  
-  else if(gridPower[i] < -(lineCapacity*(1-lineSafetyMargin))){
-    
-    setPowerBESS[i] <- -(gridPower[i]-(lineCapacity*(1-lineSafetyMargin)))
-    
-    if(setPowerBESS[i] > powerBESS){
-      
-      setPowerBESS[i] <- powerBESS
-    }
   }
 }
 
 plot(setPowerBESS, type="l")
 
 
+outputBESS <- numeric(length=length(setPowerBESS))
+
 for(i in 1:length(setPowerBESS)){
   
   if(setPowerBESS[i] >= 0){
     
-    outputBESS[i] <- ((SoC[i]-minSoC)*energyBESS)/6
+    outputBESS[i] <- ((SoC[i]-minSoC)*energyBESS)*6
     
+    if(outputBESS[i] > powerBESS){
+      
+      outputBESS[i] <- powerBESS
+    }
+
     if(setPowerBESS[i] < outputBESS[i]){
       
       outputBESS[i] <- setPowerBESS[i]
     }
   }
   
-  else if(setPowerBESS[i] < 0){
+  else{
     
-    outputBESS[i] <- ((SoC[i]-maxSoC)*energyBESS)/6
+    outputBESS[i] <- ((SoC[i]-maxSoC)*energyBESS)*6
+    
+    if(outputBESS[i] < -powerBESS){
+      
+      outputBESS[i] <- -powerBESS
+    }
     
     if(setPowerBESS[i] > outputBESS[i]){
       
@@ -59,3 +58,4 @@ for(i in 1:length(setPowerBESS)){
   
   SoC[i+1] <- SoC[i]-((outputBESS[i]/6)/(energyBESS))
 }
+
---
+++ scripts/inputs.R
@@ -0,0 +1,47 @@
+#----
+#transmission-side
+
+generation <- generationData$activePower
+load <- (loadData$activePower/4)*0.8
+
+increaseGeneration <- 0.1
+increaseLoad <- 0.0
+
+grid <- (generation*(1+increaseGeneration))-(load*(1+increaseLoad))
+
+lineCapacity <- 40
+lineSafetyMargin <- 0.01
+
+#----
+#HVAC
+
+numberHVAC <- 4000
+controlNumberHVAC <- 400
+
+powerHVAC <- 2
+
+heatingCoP <- 4
+coolingCoP <- 3.5
+
+thermCap <- 9200
+thermRes <- 50
+
+tempOut <- tempSubhourly$temp4
+
+tempRoomWanted <- 22
+tempWindow <- 2
+
+tempRoomMin <- tempRoomWanted-(tempWindow/2)
+tempRoomMax <- tempRoomWanted+(tempWindow/2)
+
+#----
+#BESS
+
+minSoC <- 0.4
+maxSoC <- 0.9
+
+powerBESS <- 1
+
+energy2powerRatio <- 2
+  
+energyBESS <- powerBESS*energy2powerRatio
